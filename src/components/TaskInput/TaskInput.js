e13cb241aa7ad3c48e3037b0ce41cd5ced4748b4
Denny Ken <denny.kmm@gmail.com>
2021-05-14 01:52:44 -0300

    create TaskInput component structure

---
+++ src/components/TaskInput/TaskInput.js
@@ -0,0 +1,12 @@
+import './TaskInput.scss'
+
+const TaskInput = () => {
+  return (
+    <div className="task-input">
+      <input></input>
+      <button>Add</button>
+    </div>
+  )
+}
+
+export default TaskInput
