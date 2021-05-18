f670631d9ec863f2f3e5ad6d1ae0022e2f369ce3
Denny Ken <denny.kmm@gmail.com>
2021-05-18 00:02:19 -0300

    improve styling

--- src/components/TaskInput/TaskInput.js
+++ src/components/TaskInput/TaskInput.js
@@ -3,7 +3,7 @@ import './TaskInput.scss'
 const TaskInput = () => {
   return (
     <div className="task-input">
-      <input></input>
+      <input placeholder="Start adding tasks by typing here"></input>
       <button>Add</button>
     </div>
   )
