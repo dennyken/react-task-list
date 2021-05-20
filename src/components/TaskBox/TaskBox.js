2cdec8149762676d5b25cf8a65368e6e3b2db556
Denny Ken <denny.kmm@gmail.com>
2021-05-20 01:05:01 -0300

    implement TaskBox logic

--- src/components/TaskBox/TaskBox.js
+++ src/components/TaskBox/TaskBox.js
@@ -1,15 +1,26 @@
+import { useState } from 'react';
+
 import BoxHeader from '../BoxHeader/BoxHeader'
 import TaskInput from '../TaskInput/TaskInput'
 import TaskList from '../TaskList/TaskList'
 import './TaskBox.scss'
 
 const TaskBox = () => {
+
+  let [tasks, setTasks] = useState([])
+
+  const addTask = (value) => {
+    let newState = [ ...tasks, value ]
+
+    setTasks(newState)
+  }
+
   return (
     <div className="task-box">
-      <BoxHeader />
+      <BoxHeader title="My tasks" />
       <main>
-        <TaskInput />
-        <TaskList />
+        <TaskInput onSubmit={addTask} />
+        <TaskList data={tasks} />
       </main>
     </div>
   )
