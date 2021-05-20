14e17cb9cd8c9ac315dbfed7f8f761cec37da08c
Denny Ken <denny.kmm@gmail.com>
2021-05-20 01:02:48 -0300

    implement TaskInput logic

--- src/components/TaskInput/TaskInput.js
+++ src/components/TaskInput/TaskInput.js
@@ -1,10 +1,41 @@
+import { useState } from 'react';
+
 import './TaskInput.scss'
 
-const TaskInput = () => {
+const TaskInput = ({ onSubmit: submit }) => {
+  
+  let [value, setValue] = useState("")
+
+  const onChangeHandler = e => {
+    setValue(e.target.value)
+  }
+
+  const onKeyDownHandler = e => {
+    if(e.key === 'Enter') submitValue(value)
+  }
+
+  const onClickHandler = () => {
+    submitValue(value)
+  }
+
+  const submitValue = (value) => {
+    if(!(value == null || value.match(/^\s*$/))) {
+      submit(value.trim())
+
+      setValue('')
+    }
+  }
+
+
   return (
     <div className="task-input">
-      <input placeholder="Start adding tasks by typing here"></input>
-      <button>Add</button>
+      <input 
+        placeholder="Start adding tasks by typing here" 
+        onChange={onChangeHandler}
+        onKeyDown={onKeyDownHandler}
+        value={value}>
+      </input>
+      <button onClick={onClickHandler}>Add</button>
     </div>
   )
 }
