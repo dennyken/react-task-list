336e6cc058d2f5db9138d2930e891d8f8e1736e1
Denny Ken <denny.kmm@gmail.com>
2021-05-20 01:06:21 -0300

    remove mock data

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -3,13 +3,6 @@ import './TaskList.scss'
 import TaskItem from '../TaskItem/TaskItem'
 
 const TaskList = ({ data }) => {
-  const values = [
-    "Find the All-Blue",
-    "Find all the poneglyphs",
-    "Find the One Piece",
-    "Become the king of all pirates"
-  ]
-
   return (
     <ol className="task-list">
       { data.map(value => <TaskItem description={value} /> )}
