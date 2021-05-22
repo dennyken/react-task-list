bb9280e1da34b07550e6f2d75a561617cc2a7a6d
Denny <denny.kmm@gmail.com>
2021-05-22 00:02:28 -0300

    change value to be rendered on TaskList

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -5,7 +5,7 @@ import TaskItem from '../TaskItem/TaskItem'
 const TaskList = ({ data }) => {
   return (
     <ol className="task-list">
-      { data.map(value => <TaskItem description={value} /> )}
+      { data.map(value => <TaskItem description={value.title} /> )}
     </ol>
   )
 }
