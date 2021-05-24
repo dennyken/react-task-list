1b2b37562c3a8898e2d648dbb60020b1a2eb0a5c
Denny <denny.kmm@gmail.com>
2021-05-24 00:32:55 -0300

    add styling to edit header

--- src/components/BoxHeader/BoxHeader.js
+++ src/components/BoxHeader/BoxHeader.js
@@ -1,10 +1,14 @@
 import { memo } from 'react'
 import './BoxHeader.scss'
+import { MdEdit } from 'react-icons/md'
 
 const BoxHeader = ({ title }) => {
   return (
     <header className="box-header">
       <h2>{title}</h2>
+      <div>
+        <MdEdit size={'100%'} />
+      </div>
     </header>
   )
 }
