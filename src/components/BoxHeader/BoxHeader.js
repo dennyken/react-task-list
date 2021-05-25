d0f90013edb3081213dae4b990542bf012d04b47
Denny <denny.kmm@gmail.com>
2021-05-25 12:47:50 -0300

    refactor BoxHeader component

--- src/components/BoxHeader/BoxHeader.js
+++ src/components/BoxHeader/BoxHeader.js
@@ -3,11 +3,19 @@ import './BoxHeader.scss'
 import { MdEdit } from 'react-icons/md'
 
 const BoxHeader = ({ title }) => {
+  const handleHeaderClick = () => {
+    console.log('BoxHeader:onClick')
+  }
+
   return (
     <header className="box-header">
-      <h2>{title}</h2>
-      <div>
-        <MdEdit size={'100%'} />
+      <div 
+        className="header-content"
+        onClick={handleHeaderClick}>
+        <h2>{title}</h2>
+        <div className="edit-icon">
+          <MdEdit size={'100%'} />
+        </div>
       </div>
     </header>
   )
