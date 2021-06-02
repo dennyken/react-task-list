23ef52e026b9069897203a9ac160bd2f35210728
Denny <denny.kmm@gmail.com>
2021-06-02 01:12:26 -0300

    create Modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,15 @@
+import Backdrop from '../Backdrop'
+import './Modal.scss'
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <Backdrop />
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Modal
